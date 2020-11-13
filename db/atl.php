d086eb2184cc89c0afc1f89007b0d8553619c482
GyanPrakash2483 <69905535+GyanPrakash2483@users.noreply.github.com>
2020-11-13 21:14:56 +0530

    Added authentication check

--- db/atl.php
+++ db/atl.php
@@ -6,14 +6,19 @@
 </head>
 <body>
 <?php
+if($_GET['auth_key'] == "admin0987") {
   if(isset($_GET['siteurl'])) {
     file_put_contents("./sitelist.txt", $_GET['siteurl']."\n", FILE_APPEND);
     file_put_contents("./uptime.txt", "95\n", FILE_APPEND);
   }
+} else {
+  die("Security tried to bypass: killing connection, die...");
+}
 ?>
 <form method="GET">
-  <input type="text" name="siteurl" />
-  <input type="submit">
+  <input type="text" name="siteurl" /><br>
+  <input type="text" name="auth_key" /><br>
+  <input type="submit" value="Add">
 </form>
 </body>
 </html>
